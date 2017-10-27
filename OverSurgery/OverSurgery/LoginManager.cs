38e07b74e482f12bc605b9c66ef3c213fa00fd91
Anderson <MRA150@student.anglia.ac.uk>
2017-10-27 14:50:38 +0100

    LoginManager class

---
+++ OverSurgery/OverSurgery/LoginManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverSurgery
+{
+    class LoginManager
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="password"></param>
+        public void CreateUser(string userName, string password)
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="password"></param>
+        public void Login(string userName, string password)
+        {
+            if (CheckLoginDetails(userName, EncryptPassword(password)))
+            {
+                // ...
+            }
+        }
+
+        /// <summary>
+        /// Matt
+        /// </summary>
+        /// <param name="password"></param>
+        public string EncryptPassword(string password)
+        {
+            return "";
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public bool CheckLoginDetails(string userName, string encryptedPassword)
+        {
+            // check parameters with stored data in DB
+            return true;
+        }
+    }
+}
