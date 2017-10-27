74418c1e31e896f84a77abedb64c5adf7fc4267e
Walter <GW390@student.anglia.ac.uk>
2017-10-27 13:49:26 +0100

    Actually added UI stuff this time

--- OverSurgery/OverSurgery/StaffList.Designer.cs
+++ OverSurgery/OverSurgery/StaffList.Designer.cs
@@ -28,11 +28,138 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.lblStaffList = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.lblSchedule = new System.Windows.Forms.Label();
+            this.lblRole = new System.Windows.Forms.Label();
+            this.lblStaffID = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.dataGridView2);
+            this.panel1.Controls.Add(this.lblStaffList);
+            this.panel1.Location = new System.Drawing.Point(152, 87);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(634, 442);
+            this.panel1.TabIndex = 1;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(29, 78);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(583, 336);
+            this.dataGridView2.TabIndex = 4;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            // 
+            // lblStaffList
+            // 
+            this.lblStaffList.AutoSize = true;
+            this.lblStaffList.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStaffList.Location = new System.Drawing.Point(24, 27);
+            this.lblStaffList.Name = "lblStaffList";
+            this.lblStaffList.Size = new System.Drawing.Size(131, 25);
+            this.lblStaffList.TabIndex = 1;
+            this.lblStaffList.Text = "STAFF LIST:";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dataGridView3);
+            this.panel2.Controls.Add(this.lblSchedule);
+            this.panel2.Controls.Add(this.lblRole);
+            this.panel2.Controls.Add(this.lblStaffID);
+            this.panel2.Controls.Add(this.lblName);
+            this.panel2.Location = new System.Drawing.Point(264, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(634, 442);
+            this.panel2.TabIndex = 4;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(34, 184);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(580, 236);
+            this.dataGridView3.TabIndex = 4;
+            // 
+            // lblSchedule
+            // 
+            this.lblSchedule.AutoSize = true;
+            this.lblSchedule.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSchedule.Location = new System.Drawing.Point(29, 141);
+            this.lblSchedule.Name = "lblSchedule";
+            this.lblSchedule.Size = new System.Drawing.Size(101, 25);
+            this.lblSchedule.TabIndex = 3;
+            this.lblSchedule.Text = "Schedule:";
+            // 
+            // lblRole
+            // 
+            this.lblRole.AutoSize = true;
+            this.lblRole.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRole.Location = new System.Drawing.Point(29, 103);
+            this.lblRole.Name = "lblRole";
+            this.lblRole.Size = new System.Drawing.Size(57, 25);
+            this.lblRole.TabIndex = 2;
+            this.lblRole.Text = "Role:";
+            // 
+            // lblStaffID
+            // 
+            this.lblStaffID.AutoSize = true;
+            this.lblStaffID.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStaffID.Location = new System.Drawing.Point(29, 65);
+            this.lblStaffID.Name = "lblStaffID";
+            this.lblStaffID.Size = new System.Drawing.Size(82, 25);
+            this.lblStaffID.TabIndex = 1;
+            this.lblStaffID.Text = "Staff ID:";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(29, 27);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(70, 25);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name:";
+            // 
+            // StaffList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(938, 616);
+            this.Controls.Add(this.panel1);
+            this.Name = "StaffList";
             this.Text = "StaffList";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Label lblSchedule;
+        private System.Windows.Forms.Label lblRole;
+        private System.Windows.Forms.Label lblStaffID;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label lblStaffList;
     }
 }
