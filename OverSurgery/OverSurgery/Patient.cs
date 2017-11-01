669be3713599fafdcdfb536b4c82d503f8f2383e
MattAnderson18 <MattAnderson18@users.noreply.github.com>
2017-11-01 13:16:58 +0000

    update DB, create Patient class

---
+++ OverSurgery/OverSurgery/Patient.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverSurgery
+{
+    class Patient
+    {
+        public void Register()
+        {
+
+        }
+
+        public void SearchByID()
+        {
+
+        }
+
+        public void SearchByNameDOB()
+        {
+
+        }
+
+        public void SearchByNameAddress()
+        {
+
+        }
+
+        public void FindSearchType()
+        {
+
+        }
+    }
+}
