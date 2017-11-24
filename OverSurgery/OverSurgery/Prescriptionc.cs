8810d3c614c605216a9af8f8fc6d6eae9649f6ba
samgeshsom <32771853+samgeshsom@users.noreply.github.com>
2017-11-24 11:33:20 +0000

    presc done

---
+++ OverSurgery/OverSurgery/Prescriptionc.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.IO;
+
+namespace OverSurgery
+
+{
+    #region Prescriptionc
+    public sealed class Prescriptionc
+    {
+        #region Singleton
+
+        static Prescriptionc instance = null;
+        static readonly object myLock = new object();
+
+
+
+        public static Prescriptionc Instance
+        {
+            get
+            {
+                lock (myLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Prescriptionc();
+
+                    }
+                }
+                return instance;
+            }
+
+
+        }
+
+        #endregion
+
+        #region Attributes
+
+        private prescriptionsform prescriptionform = new prescriptionsform();
+
+        private Patient activePatient;
+
+
+
+        #endregion
+
+        #region Properties
+        public Patient ActivePatient
+        {
+            get { return activePatient; }
+            set
+            {
+                activePatient = value;
+                //PrescriptionInfor.UpdateActivePatientLabels();
+                Prescriptionc.Instance.UpdatePrescriptionsDataGrid();
+
+            }
+
+        }
+
+        public prescriptionsform prescriptionsform
+        {
+            get { return prescriptionsform; }
+            set
+            {
+                prescriptionsform = value;
+
+            }
+
+        }
+
+
+
+        #endregion
+
+        #region Update Methods
+        public void UpdatePrescriptionsDataGrid()//will add soon sam
+        {
+            if (ActivePatient != null)
+            {
+                prescriptionsform.PrescriptionsGrid.DataSource = LoadPrescriptions().Tables[0];
+            }
+            else
+            {
+                prescriptionsform.PrescriptionsGrid.DataSource = LoadPrescriptions().Tables[0];
+            }
+
+        }
+        #endregion
+
+        #region GetDataset Methods
+        public DataSet LoadMedNameAndNotes(string medID)
+        {
+
+            string sql = @"SELECT MedName, Notes  FROM Meds WHERE Id = '" + medID + "'";
+
+            return DatabaseConnection.getDatabaseConnectionInstance().getDataSet(sql);
+        }
+
+
+        //public Dataser needed for string name ...
+
+        public DataSet LoadPrescriptions(string id)
+        {
+            DataSet ds = new DataSet();
+            string sql = @"SELECT * FROM PatientsMeds WHERE PatientID = '" + id + "' ";
+
+            ds = DatabaseConnection.getDatabaseConnectionInstance().getDataSet(sql);
+            return ds;
+        }
+
+        public DataSet LoadPrescriptions()
+        {
+            DataSet ds = new DataSet();
+            string sql = @"SELECT * FROM  PatientsMeds ";
+
+            ds = DatabaseConnection.getDatabaseConnectionInstance().getDataSet(sql);
+            return ds;
+        }
+        public void ExtendPrescrption(string medID, string patientID, string extendDate)
+        {
+
+            DataSet ds = new DataSet();
+            string sql = @"UPDATE PatientsMeds SET ExtentionDate = '" + extendDate + "' WHERE MedId = '" + medID + "' AND PatientID = '" + patientID + "'";
+
+            ds = DatabaseConnection.getDatabaseConnectionInstance().getDataSet(sql);
+
+        }
+
+        public void DeleteExtention(string medID, string patientId)
+        {
+            DataSet ds = new DataSet();
+            string sql = @"UPDATE PatientsMeds SET ExtentionDate = NULL WHERE MedId = '" + medID + "' AND PatientID = '" + patientId + "'";
+
+
+            ds = DatabaseConnection.getDatabaseConnectionInstance().getDataSet(sql);
+        }
+        #endregion
+    }
+    #endregion
+}
