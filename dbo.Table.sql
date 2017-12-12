9443d0f712e164d502e7dd5fec6fcb0c68939339
Walter <GW390@student.anglia.ac.uk>
2017-12-12 11:35:58 +0000

    Added EERD Diagram

--- dbo.Table.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[Medicine] (
-    [Id]      INT         IDENTITY (1, 1) NOT NULL,
-    [MedName] NCHAR (30)  NOT NULL,
-    [Length]   NCHAR (100) NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
